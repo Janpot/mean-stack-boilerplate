423cfd9a01c8420f20677d2540c0bd53bc5356ba
Jan Potoms <potoms.jan@gmail.com>
2014-01-15 22:41:29 +0100

    Remove jquery dependency

--- app/js/directives/viewTitle.js
+++ app/js/directives/viewTitle.js
@@ -1,12 +1,15 @@
 /*global angular*/
 
-angular.module('app').directive('viewTitle', function () {
-  'use strict';
-  
-  return function (scope, elm, attrs) {
-    attrs.$observe('viewTitle', function (value) {
-      angular.element('html head title').text(value);
-    });
-  };
-  
-});
+angular.module('app')
+  .directive('viewTitle', function (
+    $document
+  ) {
+    'use strict';
+    
+    return function (scope, elm, attrs) {
+      attrs.$observe('viewTitle', function (value) {
+        $document.prop('title', value);
+      });
+    };
+    
+  });
--- app/js/services/authentication.js
+++ app/js/services/authentication.js
@@ -1,16 +1,23 @@
 /*global angular*/
 
 angular.module('app').factory('authentication', function (
+  $document,
   $window,
   $log
 ) {
   'use strict';
+    
+  var meta = {},
+      user = null;
   
-  var userJson = angular.element('head meta[name=user]')[0].content,
-      user     = null;
+  angular.forEach($document.find('meta'), function (metaTag) {
+    if (metaTag.name) {
+      meta[metaTag.name] = metaTag.content;
+    }
+  });
     
   try {
-    user = JSON.parse(userJson);
+    user = JSON.parse(meta.user);
   } catch (e) {
     $log.error(e);
   }
